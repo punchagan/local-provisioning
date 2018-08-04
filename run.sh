404776f99a4c3a9889190fe79d15cbd89ba95ae4
Puneeth Chaganti <punchagan@muse-amuse.in>
2018-08-04 19:38:48 +0530

    run: Switch back to python2

--- run.sh
+++ run.sh
@@ -2,9 +2,14 @@
 
 set -e
 
-if [ -z $(which pip3) ]; then
+if [ -z $(which python) ]; then
+    echo "Installing python..."
+    sudo apt update
+fi
+
+if [ -z $(which pip) ]; then
     echo "Installing pip..."
-    sudo apt install -y python3-pip
+    sudo apt install -y python-pip
 fi
 
 if [ -z $(which git) ]; then
@@ -12,6 +17,7 @@ if [ -z $(which git) ]; then
     sudo apt install -y git
 fi
 
-pip3 install -r requirements.txt --user
+sudo apt install -y python python-apt
+pip install -r requirements.txt --user
 
 $HOME/.local/bin/ansible-playbook setup.yml -i HOSTS --module-path ./ansible_modules --ask-become-pass --extra-vars "@config.yml"
