bc350190304fe13c15670385b3543dd56f38303f
Puneeth Chaganti <punchagan@muse-amuse.in>
2018-08-04 16:23:24 +0530

    run: Make the script more automation friendly

--- run.sh
+++ run.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
+set -e
+
 if [ -z $(which pip3) ]; then
     echo "Installing pip..."
-    sudo apt install python3-pip
+    sudo apt install -y python3-pip
 fi
 
 if [ -z $(which git) ]; then
     echo "Installing git..."
-    sudo apt install git
+    sudo apt install -y git
 fi
 
 pip3 install -r requirements.txt --user
 
-ansible-playbook setup.yml -i HOSTS --module-path ./ansible_modules --ask-become-pass --extra-vars "@config.yml"
+$HOME/.local/bin/ansible-playbook setup.yml -i HOSTS --module-path ./ansible_modules --ask-become-pass --extra-vars "@config.yml"
