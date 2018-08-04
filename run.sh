63a428f2af0c8d6eb619b1c09a6cd3287d0420f0
Puneeth Chaganti <punchagan@muse-amuse.in>
2018-08-04 15:07:42 +0530

    run: Ensure that git is installed

--- run.sh
+++ run.sh
@@ -5,6 +5,11 @@ if [ -z $(which pip3) ]; then
     sudo apt install python3-pip
 fi
 
+if [ -z $(which git) ]; then
+    echo "Installing git..."
+    sudo apt install git
+fi
+
 pip3 install -r requirements.txt --user
 
 ansible-playbook setup.yml -i HOSTS --module-path ./ansible_modules --ask-become-pass --extra-vars "@config.yml"
